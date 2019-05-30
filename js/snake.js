fc51f39de2251a44252397d9f5525eeaf581301e
soompy <sooper1137@gmail.com>
2019-05-31 01:48:30 +0900

    게임판과 뱀 그리기

--- js/snake.js
+++ js/snake.js
@@ -1,25 +1,29 @@
-$(function () {
-    var board = $("#snakehouse");
-    var snake = $(".snake");
-    var timer = 0;
-    var delay = 500;
+var body = document.getElementsByClassName("snake"); // snake 클래스
+var house = document.getElementById("snakehouse"); // snakehouse 아이디
+var board = 20; // 가로, 세로 칸 수
 
-    // timer = setInterval(game,delay);
-    // console.log(snake);
+var snake = new Array(); // 뱀 몸통
 
-
-    // function game() {
-    //     console.log('call game');
-    //     snake.animate({right:"20px"},delay);
-    // }
-    // game();
-
-})
+var apple = new Array();
+var direction = "right"; // 이동방향
 
 
+function init(){ // 칸
+    snake = [];
+    arr.push([0,1]);
+    onsnake();
+}
 
-$("#start").click(function () {
-    $(".snake").animate({right:"20px"},1000);
-});
+function onsnake() { // 뱀 그리기
+    $("#snakehouse td").removeClass('snake');
+    for(var i = 0; i <= board; i++){
 
+    }
+}
 
+function move() { // 뱀 이동
+     var head = new Array();
+     head[0] = snake[0][0];
+     head[1] = snake[0][1];
+
+}
